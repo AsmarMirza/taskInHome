f8bdc70d9157ca92554c18ae4eb0dd4e897d021a
Gamze03 <qemzemirkisiyeva90@gmail.com>
2024-05-02 19:27:29 +0400

    file modified

--- basket_context/src/context/MainProvider.jsx
+++ basket_context/src/context/MainProvider.jsx
@@ -6,7 +6,11 @@ export const MainContext=createContext()
 function MainProvider({children}) {
     const [basket, setBasket] = useState([])
     function addBasket(item){
-setBasket([...basket,item])
+        const index=basket.findIndex((x)=>x.id===item.id)
+        if (index!==-1) {
+            return
+        }
+        setBasket([...basket,item])
     }
     function removeBasket(item){
         setBasket(basket.filter((x)=>x.id!==item.id))
